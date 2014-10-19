e2159a868156b28a8a41463326c3d6390ca9dfe3
James Finlay <jtfinlay@ualberta.ca>
2014-10-19 14:20:46 -0600

    Memory fixes.

--- src/lib_insertionsort.c
+++ src/lib_insertionsort.c
@@ -7,20 +7,20 @@ JNIEXPORT jintArray JNICALL Java_InsertionSort_insertsort
 (JNIEnv *env, jobject object, jintArray values)
 {
     jsize len;
-    jint x, *arr;
-    int i, j, mem;
+    jint x, *arr, *mem;
+    int i, j;
     jboolean *is_copy;
     jintArray result;
 
     len = (*env)->GetArrayLength(env, values);
     arr = (jint *) (*env)->GetIntArrayElements(env, values, is_copy);
-    mem += 9;
+    *mem += 9;
     if (arr == NULL)
     {
         printf("Cannot obtain array from JVM\n");
         exit(0);
     }
-    mem+=1;
+    *mem+=1;
 
     for (i = 1; i < len; i++)
     {
@@ -30,15 +30,15 @@ JNIEXPORT jintArray JNICALL Java_InsertionSort_insertsort
         {
             arr[j] = arr[j-1];
             j--;
-            mem+=5;
+            *mem+=5;
         }
         arr[j] = x;
-        mem+=16;
+        *mem+=16;
     }
 
     result = (*env)->NewIntArray(env, len+1);
     (*env)->SetIntArrayRegion(env, result, 0, len, arr);
-    mem+=9;
+    *mem+=9;
     (*env)->SetIntArrayRegion(env, result, len, len+1, mem);
 
     return result;
